bbc2ddb59a47a2163a39209ed42387a4217db274
Denys Menfredy <denys.menfredy7@gmail.com>
2020-12-06 19:48:43 -0300

    tf-cpu

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ st.text("Faça o upload de uma imagem de dermatoscopia para identificar se o can
 uploaded_file = st.file_uploader("Escolha uma image ...", type="jpg")
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    st.image(image, caption='Imagem escolhida.', width=600, height=400, use_column_width=False)
+    st.image(image, caption='Imagem escolhida.', use_column_width=True)
     st.write("")
     st.write("Classifying...")
     label = skin_cancer_classification(image, 'efficientnetb0_89acc.h5')
