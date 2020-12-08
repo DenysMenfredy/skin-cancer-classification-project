b0d30dd7897c60475e357c27b8715c990ebbb274
Denys Menfredy <denys.menfredy7@gmail.com>
2020-12-08 09:47:56 -0300

    alteracoes

--- app.py
+++ app.py
@@ -3,17 +3,17 @@ from img_classification import skin_cancer_classification
 from PIL import Image
 
 st.title("Skin Cancer Classification")
-st.header("Skin Cancer Classification Example")
-st.text("Faça o upload de uma imagem de dermatoscopia para identificar se o cancer é maligno ou benigno")
+st.header("Detecção de Câncer de pele do tipo melanoma.
+st.text("Faça o upload de uma imagem de dermatoscopia")
 
 uploaded_file = st.file_uploader("Escolha uma imagem...", type="jpg")
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption='Imagem escolhida.', use_column_width=True)
     st.write("")
-    st.write("Classifying...")
+    st.write("Classificando...")
     label = skin_cancer_classification(image, 'efficientnetb0_89acc.h5')
     if label == 0:
-        st.write("Cancer Benigno")
+        st.write("Melanoma Benigno")
     else:
-        st.write("Cancer Maligno")
+        st.write("Melanoma Maligno")
