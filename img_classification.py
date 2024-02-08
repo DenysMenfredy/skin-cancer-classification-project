7a924a8e57ad70f8b5d8f7a9bca6120722d9e5de
Denys Menfredy <denys.menfredy7@gmail.com>
2024-02-08 15:26:01 -0300

    fix old requiriments versions

--- img_classification.py
+++ img_classification.py
@@ -12,7 +12,7 @@ def skin_cancer_classification(img, weights_file):
     image = img
     #image sizing
     size = (224, 224)
-    image = ImageOps.fit(image, size, Image.ANTIALIAS)
+    image = ImageOps.fit(image, size, Image.LANCZOS)
     
     #turn the image into a numpy array
     image_array = np.asarray(image)
